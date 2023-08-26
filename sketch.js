f029ea47ee482f50c5eed05218523528dbc12430
Ismail Silat <ismailsilat7@gmail.com>
2023-08-27 02:19:07 +0500

    add hexToRgba function & implement it to change box-shadow color

--- sketch.js
+++ sketch.js
@@ -4,13 +4,16 @@ let sketchMode = 'classic';
 let sketchColor = 'black';
 const colorPicker = document.querySelector('.c-picker');
 let darkIntensity = 0.1;
-colorPicker.addEventListener('input', (e) => {
-  sketchColor = e.target.value;
-});
 const sizeInput = document.querySelector('#size');
 const submit = document.querySelector('.submit');
 sizeInput.value = "16";
 let gridInitialized = false; // Variable to track if the grid has been initialized
+
+colorPicker.addEventListener('input', (e) => {
+  sketchColor = e.target.value;
+  colorPicker.style.boxShadow = `0px 0px 10px 5px ${hexToRgba(sketchColor, 0.2)}`;
+  sketchMode = 'mix';
+});
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
@@ -27,6 +30,8 @@ submit.addEventListener('click', (e) => {
     gridCol = sizeValue;
     makeGrid(sizeValue, sizeValue);
     gridInitialized = false; // Set the gridInitialized variable to true
+    const dimensions = document.querySelector('.dimensions')
+    dimensions.textContent=`(${sizeValue}x${sizeValue})`
   }
 });
 
@@ -43,7 +48,9 @@ function makeGrid(gridRow,gridCol) {
       box.addEventListener('mouseover', () => {
         switch (sketchMode) {
           case 'classic':
-            box.style.backgroundColor = sketchColor;
+            box.style.backgroundColor = 'black';
+            colorPicker.value = 'black';
+            colorPicker.style.boxShadow = `0px 0px 10px 5px ${hexToRgba('#000000', 0.2)}`;
             break;
           case 'rainbow':
             box.style.backgroundColor = randomColor();
@@ -58,7 +65,7 @@ function makeGrid(gridRow,gridCol) {
             }
             break;
           default:
-            box.style.backgroundColor = 'black';
+            box.style.backgroundColor = sketchColor;
         }
       })
     }
@@ -87,6 +94,29 @@ function clearGrid() {
 function clearGridForNewSize() {
   const gridContainer = document.querySelector('.gridContainer');
   gridContainer.innerHTML = ''; // Remove all child elements (boxes)
+}
+
+function hexToRgba(hex, alpha) {
+  // Remove the '#' symbol if it exists
+  hex = hex.replace('#', '');
+
+  // Parse the hexadecimal values for R, G, and B
+  const red = parseInt(hex.slice(0, 2), 16);
+  const green = parseInt(hex.slice(2, 4), 16);
+  const blue = parseInt(hex.slice(4, 6), 16);
+
+  // Create the RGBA color string
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+}
+
+function scrollToSize() {
+  const target = document.getElementById('form-container');
+  target.scrollIntoView();
+}
+
+function scrollToSketch() {
+  const target = document.getElementById('sketch-section');
+  target.scrollIntoView();
 }
 
 // Initially create the grid
